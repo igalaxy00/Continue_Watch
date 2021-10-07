fd5384a32786d803ab8be9438d531b11fbca7673
Igor <i.galaxy00@gmail.com>
2021-10-07 11:43:05 +0300

    Initial commit

---
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -0,0 +1,64 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    var secondsElapsed: Int = 0
+    lateinit var textSecondsElapsed: TextView
+
+    var MyThread : Boolean = true
+
+    var backgroundThread = Thread {
+        while (true) {
+            if (MyThread) secondsElapsed++
+            textSecondsElapsed.post {
+                textSecondsElapsed.setText(getString(R.string.seconds) + secondsElapsed)
+            }
+            Thread.sleep(1000)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        MyThread = true
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        MyThread = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        MyThread = false
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MyThread = false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        backgroundThread.start()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run{putInt(SECONDS, secondsElapsed)}
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.run{secondsElapsed = getInt(SECONDS)}
+    }
+
+
+    companion object { const val SECONDS = "Seconds" }
+
+}
